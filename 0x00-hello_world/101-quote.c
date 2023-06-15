bfed018f670130bb31e4b6cde8b32211a5616203
BTWanyangu <bryantitus06@gmail.com>
2023-06-15 11:24:31 -0700

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - program printing to std error
- * Reaturn: 1 (success)
+ * main - print line next to std error
+ * Return: 1 (success)
  */
 int main(void)
-{
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
