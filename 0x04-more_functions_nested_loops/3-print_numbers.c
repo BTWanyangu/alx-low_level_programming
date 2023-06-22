d2c97eeb154c552434277c646e4e977d234b8201
BTWanyangu <bryantitus06@gmail.com>
2023-06-22 04:21:28 -0700

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: 0 (success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; && i < 10; i++)
+{
+_putchar('0' + i);
+}
+_putchar('\n')
+}
+
