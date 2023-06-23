a7149cf22a24f1487bd48973f9784301ebd14319
BTWanyangu <bryantitus06@gmail.com>
2023-06-23 11:17:00 -0700

    coreected

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,25 +3,23 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints numbers in a loop and avoids infinite loop
+ * Description: Causes an infinite loop
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
 
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i + '0'); /* Print the numerical value of i */
-		i++; /* Increment i to avoid infinite loop */
-	}
-
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
+    /*
+     * while (i < 10)
+     * {
+     *     putchar(i);
+     * }
+     */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
 
