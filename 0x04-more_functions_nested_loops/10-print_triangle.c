2b1773a018bd7e648a2c53b7262b80609d446fb8
BTWanyangu <bryantitus06@gmail.com>
2023-06-23 02:50:27 -0700

    added 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,12 +7,12 @@
  */
 void print_triangle(int size)
 {
-int row, hashes;
+int row, hashes, spaces;
 if (size <= 10)
 {
-
 for (row = 1; row <= size; row++)
 {
+for (spaces = size - row; spaces >= 1; spaces--)
 _putchar(' ');
 for (hashes = 1; hashes <= row; hashes++)
 {
