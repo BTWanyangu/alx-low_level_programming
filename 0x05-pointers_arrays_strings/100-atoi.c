db38ee3576d047ed7bd4e8fa17ed8f51ec8eda34
BTWanyangu <bryantitus06@gmail.com>
2023-06-26 04:30:40 -0700

    added 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	/* Check for the sign */
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	/* Convert string to integer */
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = (result * 10) + (s[i] - '0');
+		}
+		else
+		{
+			break; /* Stop conversion if non-digit character is encountered */
+		}
+
+		i++;
+	}
+
+	return (result * sign);
+}
+
