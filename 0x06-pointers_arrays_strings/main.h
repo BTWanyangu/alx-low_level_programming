6dcb8ef8d28e891103974d8388a02e534ca62aa0
BTWanyangu <bryantitus06@gmail.com>
2023-06-29 09:59:33 -0700

    added main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -10,4 +11,7 @@ char *string_toupper(char *);
 char *cap_string(char *)i;
 char *rot13(char *);
 void print_number(int n);
+char *leet(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size r);
+void print_buffer(char *b, int size);
 #endif
