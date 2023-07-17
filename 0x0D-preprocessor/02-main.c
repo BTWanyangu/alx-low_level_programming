73507485322c9e22103dbda2092930bbb377405c
BTWanyangu <bryantitus06@gmail.com>
2023-07-17 05:13:34 -0700

    added 2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program.
+ * It prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
+
