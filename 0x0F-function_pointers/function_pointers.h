c304fb14090529c73f9aeebf56801bbf79e74cfb
BTWanyangu <bryantitus06@gmail.com>
2023-07-19 11:17:50 -0700

    added int_index.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -24,6 +24,7 @@ void print_elem_hex(int elem);
  * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
 
